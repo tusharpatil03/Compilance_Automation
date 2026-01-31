3ddb48f4c8ea5b440f74df39292fe2002618814c
tusharpatil03 <tusha2149@gmail.com>
2026-01-31 11:30:01 +0530

    cors origin added

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,28 @@
 import express from "express";
 import "dotenv/config";
 import userRouter from "./modules/customers/routes";
+import cors from "cors";
+import tenantRoutes from "./modules/tenant/routes";
 
 const app = express();
 app.use(express.json());
 
+//cors
+const allowedOrigins = ["http://localhost:5173"];
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true,
+}));
+
 app.use("/user", userRouter);
+app.use("/tenant", tenantRoutes);
 
 export default app;
