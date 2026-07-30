ce70408d31b6693e7530f2b6a3bcb5857154e0dd
tusharpatil03 <tusha2149@gmail.com>
2026-07-30 11:07:02 +0530

    fix: added vercel proxy to fix cors isssue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({
-  server: {
-    port: 4000,
-    strictPort: true,
-  },
+export default defineConfig(({ mode }) => {
+  // Load env file based on the current mode (development, production, etc.)
+  // The third argument '' loads all variables regardless of the VITE_ prefix
+  const env = loadEnv(mode, process.cwd(), "");
 
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
     },
-  },
+    server: {
+      port: Number(env.VITE_PORT) || 4000,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL, // Use the environment variable for the API base URL
+          changeOrigin: true, // Needed for virtual hosted sites
+          rewrite: (path) => path.replace(/^\/api/, "") // Remove /api prefix when forwarding the request
+        }
+      }
+    }
+  }
 });
